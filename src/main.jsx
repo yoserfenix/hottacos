87c775beb21fc39defb48801dc54eaffe688048d
yoserfenix <yoserfenix@gmail.com>
2024-12-02 23:05:30 -0500

    version 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-// src/index.jsx
+// src/main.jsx
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';  // Se importa 'react-dom/client' para React 18
 import { Provider } from 'react-redux';
 import store from './state/store';
 import App from './App';
 
-ReactDOM.render(
+// Crear el root usando React 18
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -17,7 +17,34 @@ const AboutUs = () => {
         </div>
       </section>
 
-
+      {/* Nuestra Historia */}
+      <section className="section history">
+        <div className="text-content">
+          <h2>Un pedacito de México en Canadá</h2>
+          <p>
+            Desde enero de 2020, Hot Tacos ha llevado el auténtico sabor de México a Canadá, 
+            creando un espacio donde todos son bienvenidos a disfrutar de una experiencia gastronómica inolvidable. 
+            Aquí, celebramos la riqueza de la cocina mexicana a través de platillos como nuestros tacos al pastor 
+            y las icónicas Papas a la Hot Tacos, preparados con ingredientes frescos y recetas tradicionales.
+          </p>
+          <p>
+            Hot Tacos es más que un restaurante; es un lugar donde la cultura mexicana cobra vida en cada detalle. 
+            Nos enorgullece ser un puente entre nuestras raíces y nuevas experiencias, invitando a todos, sin importar su origen, 
+            a descubrir la magia de México en cada bocado. Desde sus inicios, Hot Tacos ha crecido exponencialmente, 
+            posicionándose como un referente en la región y en redes sociales.
+          </p>
+          <p>
+            Hemos participado en festivales latinos y actividades comunitarias, apoyado equipos juveniles de hockey, 
+            boxeo y lucha libre, y construido conexiones con nuestros clientes. En marzo de 2024, nos expandimos a Windsor, 
+            donde rápidamente nos convertimos en un punto de encuentro destacado, ganando excelentes reseñas y atrayendo a influencers.
+          </p>
+        </div>
+        <img
+          src="/assets/history.jpg"
+          alt="Historia del restaurante"
+          className="section-image"
+        />
+      </section>
       <MissionVision />
       <Values />
       <Special />
